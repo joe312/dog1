80e5cda10172a69324b81f7120ff5687a099cc81
joe <joe312@sent.com>
2015-04-03 07:40:55 +0100

    my first committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dog1::Application.config.secret_token = 'c17e9d1769e69ba5fa143c134a7eba84866115c9f4e7f7c24fd10d2f6162254333a6d3e2febf649696f4e3974b36b3ba12839d22bf5965e1e8b4b3176922c5a5'
